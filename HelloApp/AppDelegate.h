d916c6a4b4ef6e8f1f240094fdbb28f35c991a68
Marlou <onoitzmalo@gmail.com>
2012-07-31 13:40:35 -0500

    Initial Commit

---
+++ HelloApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloApp
+//
+//  Created by Marlou de Guzman on 7/31/12.
+//  Copyright (c) 2012 Marlou de Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
